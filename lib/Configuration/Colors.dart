3533cda4c9977399708db7be528efaa2bb474230
IAftabRehman <iamaftabrehman@gmail.com>
2025-09-06 20:13:07 +0500

    Set Color at Colors.dart

--- lib/Configuration/Colors.dart
+++ lib/Configuration/Colors.dart
@@ -1,5 +1,9 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class color{
   static const Color background = Color(0xff090040);
+  static const Color dayModePrimaryColor = Colors.white;
+  static const Color nightModePrimaryColor = Colors.black54;
 }
